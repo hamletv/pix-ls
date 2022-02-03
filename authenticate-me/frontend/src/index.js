f7f48dda798b381573e47b338de112c014e654cf
Hamlet <hamlet.villa@gmail.com>
2022-02-03 17:06:53 -0500

    frontend setup complete

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -5,10 +5,18 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 // redux store available
 const store = configureStore();
 if(process.env.NODE_ENV !== 'production'){
+  window.store = store;
+}
+
+// for development only
+if(process.env.NODE_ENV !== 'production'){
+  restoreCSRF();
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
