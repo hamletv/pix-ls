6bf86ac161848357ebafcf50aa6c137876bdbb0c
Hamlet <hamlet.villa@gmail.com>
2022-02-08 13:46:09 -0500

    seeder files update

--- backend/db/seeders/20220207221807-comments.js
+++ backend/db/seeders/20220207221807-comments.js
@@ -6,32 +6,44 @@ module.exports = {
       {
         userId: 1,
         imageId: 1,
-        comment: 'I love this photo, amazing detail!'
+        comment: 'I love this photo, amazing detail!',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         userId: 1,
         imageId: 2,
-        comment: 'That\'s one lucky eagle...and one unlucky fish.'
+        comment: 'That\'s one lucky eagle...and one unlucky fish.',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         userId: 1,
         imageId: 3,
-        comment: 'Simply beautiful.'
+        comment: 'Simply beautiful.',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         userId: 2,
         imageId: 2,
-        comment: 'It\'s 5 o\'clock somewhere!!! Drink on.'
+        comment: 'It\'s 5 o\'clock somewhere!!! Drink on.',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         userId: 2,
         imageId: 5,
-        comment: 'I wish I could just pull that pizza out of the screen!.'
+        comment: 'I wish I could just pull that pizza out of the screen!.',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         userId: 2,
         imageId: 6,
-        comment: 'Fresh pasta is so time consuming but so worth it when you take that first bite...and the 20th bite too.'
+        comment: 'Fresh pasta is so time consuming but so worth it when you take that first bite...and the 20th bite too.',
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ], {});
   },
