6ccd5cb2eb493dfd35ad5f451fe1f9e87eee5254
Hamlet <hamlet.villa@gmail.com>
2022-02-07 23:22:54 -0500

    started routes

---
+++ backend/routes/api/images.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { Images } = require('../../db/models');
+const router = express.Router();
+
+
+router.get('/images', asyncHandler (async (req, res) => {
+
+}))
