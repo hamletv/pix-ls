456e61f2c6130727f4ff5b57ed3c8e5e7672112c
Hamlet <hamlet.villa@gmail.com>
2022-02-10 23:56:13 -0500

    splash page image, reducer edits

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -1,14 +1,26 @@
 
 
 const SplashPage = () => {
+    const myStyle={
+        backgroundImage:
+ "url('https://images.unsplash.com/photo-1643133277936-9f93d8792522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')",
+        height:'100vh',
+        marginTop:'-70px',
+        fontSize:'50px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+    };
     return (
         <>
-            <div>
-                <h1>Splash Page</h1>
-            </div>
+        <div style={myStyle}>
+            <h1>Pixls</h1>
+        </div>
         </>
-    )
+    );
 }
 
 
+{/* <div>
+    <h1>Splash Page</h1>
+</div> */}
 export default SplashPage;
